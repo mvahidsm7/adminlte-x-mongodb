a53ba56447c586cbe5ddb084041d15ea5a46e076
Muhammad Vahid Sumantri <mvahidsm7@gmail.com>
2026-06-15 16:39:32 +0700

    refactor: move AppShell to (main) layout group, prepare for (auth) group

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import { Plus_Jakarta_Sans } from 'next/font/google'
 import './globals.css'
-import AppShell from '@/components/layout/AppShell'
 
 const plusJakarta = Plus_Jakarta_Sans({
   subsets: ['latin'],
@@ -23,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={plusJakarta.variable}>
       <body className="font-sans antialiased">
-        <AppShell>{children}</AppShell>
+        {children}
       </body>
     </html>
   )
